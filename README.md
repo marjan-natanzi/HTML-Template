b7eb45af140969a521c13da3946b2a9f608b646a
Ali <ali@ali@firooz.com>
2020-04-07 21:28:04 +0430

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Elsta HTML Template
---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $('.slider').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+  });
+
+  document.getElementById("contact-us-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    var formData = {
+      name : document.getElementById("name").value,
+      email : document.getElementById("email").value,
+      message : document.getElementById("message").value
+    };
+    console.log(formData);
+    document.getElementById("contact-us-form").reset();
+  });
+  
+});
